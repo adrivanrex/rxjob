bd008b9ab916c1bd2efdeafe3a22a5b076817be1
adrivanrex <adrivanrex@gmail.com>
2017-12-28 17:32:15 +0800

    added onclick graph

--- js/controllers.js
+++ js/controllers.js
@@ -1072,6 +1072,7 @@ function MainCtrl($window, $scope, $firebaseAuth, $location, $firebaseObject, $t
         showMessages($location.search().id);
 
         if ($location.path('/dashboards/marketInfo')) {
+        	showGraph();
             orders();
             acceptedOrders();
         }
